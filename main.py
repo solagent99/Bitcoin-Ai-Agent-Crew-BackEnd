e0016298b4387ecf9a565f1eb3005eaf1f375401
solagent99 <davidanderson9469@gmail.com>
2024-10-31 14:03:02 -0700

    fix: try deferring route loading to init endpoint

--- main.py
+++ main.py
@@ -1,51 +1,40 @@
-import asyncio
-from contextlib import asynccontextmanager
-from fastapi import APIRouter, FastAPI
+from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-from api import crew
-from api import chat
 
 # set up a simple app to respond to server health check
-pass_health_check = FastAPI()
+app = FastAPI()
+
+# setup CORS origins
+# TODO: use after confirming it works
+cors_origins = [
+    "https://sprint.aibtc.dev",
+    "https://aibtcdev-frontend.replit.app",
+]
 
 
-@pass_health_check.get("/")
+# setup middleware to allow CORS
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+@app.get("/")
 async def health():
     return {"message": "A healthy server is a happy server!"}
 
 
-root_router = APIRouter()
+@app.get("/initialize")
+async def initialize():
+    global routes_initialized
 
+    if not routes_initialized:
+        from api import crew
 
-@root_router.get("/")
-async def root():
-    return {"message": "CrewAI execution API is running!"}
-
-
-@asynccontextmanager
-async def lifespan(app: FastAPI):
-    # sleep to let health check complete
-    await asyncio.sleep(10)
-    # load the routes
-    app.include_router(root_router)
-    app.include_router(crew.router)
-    app.include_router(chat.router)
-    yield
-    # can add cleanup code here
-
-
-app = FastAPI(lifespan=lifespan)
-
-# Allow requests from the Next.js frontend
-# For local dev: allow_orgins=["http://localhost:3000"]
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=[
-        "https://aibtcdev-frontend.replit.app",
-        "https://sprint.aibtc.dev",
-        "*",
-    ],  # Allow access from frontend
-    allow_credentials=True,
-    allow_methods=["*"],  # Allows all methods (GET, POST, etc.)
-    allow_headers=["*"],  # Allows all headers
-)
+        app.include_router(crew.router)
+        routes_initialized = True
+        return {"message": "Routes loaded and initialized!"}
+    return {"message": "Routes already initialized!"}
